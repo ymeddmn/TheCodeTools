97ac7162bb2111fa52859edc38527929d732113a
mage <1275054668@qq.com>
2021-04-08 14:47:21 +0800

    增加几个工具类和readme

---
+++ TheCodeToolsProject/app/src/main/java/com/sitech/hsh/thecodetoolsproject/tools/MacAddressUtils.java
@@ -0,0 +1,189 @@
+package com.sitech.hsh.thecodetoolsproject.tools;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.text.TextUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * author  :mayong
+ * function:
+ * date    :2020-04-23
+ **/
+public class MacAddressUtils {
+    public static String getmac(Context context) {
+         String mac = getMacAddress();//获取有线mac地址
+        if(TextUtils.isEmpty(mac)){
+            mac= getMacAddress(context);
+        }
+        final String result = TextUtils.isEmpty(mac) ? "-1" : mac;
+        return result;
+    }
+
+    private static String getMacAddress(Context context) {
+        String mac = "02:00:00:00:00:00";
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            mac = getMacDefault(context);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            mac = getMacFromFile();
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            mac = getMacFromHardware();
+        }
+        return mac;
+    }
+
+    /**
+     * Android  6.0 之前（不包括6.0）
+     * 必须的权限  <uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
+     *
+     * @param context
+     * @return
+     */
+    private static String getMacDefault(Context context) {
+        String mac = "02:00:00:00:00:00";
+        if (context == null) {
+            return mac;
+        }
+
+        WifiManager wifi = (WifiManager) context.getApplicationContext()
+                .getSystemService(Context.WIFI_SERVICE);
+        if (wifi == null) {
+            return mac;
+        }
+        WifiInfo info = null;
+        try {
+            info = wifi.getConnectionInfo();
+        } catch (Exception e) {
+        }
+        if (info == null) {
+            return null;
+        }
+        mac = info.getMacAddress();
+        if (!TextUtils.isEmpty(mac)) {
+            mac = mac.toUpperCase(Locale.ENGLISH);
+        }
+        return mac;
+    }
+
+    /**
+     * Android 6.0（包括） - Android 7.0（不包括）
+     *
+     * @return
+     */
+    private static String getMacFromFile() {
+        String WifiAddress = "02:00:00:00:00:00";
+        try {
+            WifiAddress = new BufferedReader(new FileReader(new File("/sys/class/net/wlan0/address"))).readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return WifiAddress;
+    }
+
+    /**
+     * 遍历循环所有的网络接口，找到接口是 wlan0
+     * 必须的权限 <uses-permission android:name="android.permission.INTERNET" />
+     *
+     * @return
+     */
+    private static String getMacFromHardware() {
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(String.format("%02X:", b));
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "02:00:00:00:00:00";
+    }
+
+
+    /**
+     * 获取有线（以太网）mac地址
+     *
+     * @return
+     */
+    private static String getMacAddress() {
+        try {
+            String strMacAddr = null;
+            try {
+                InetAddress ip = getLocalInetAddress();
+                byte[] b = NetworkInterface.getByInetAddress(ip)
+                        .getHardwareAddress();
+                StringBuffer buffer = new StringBuffer();
+                for (int i = 0; i < b.length; i++) {
+                    if (i != 0) {
+                        buffer.append(':');
+                    }
+                    String str = Integer.toHexString(b[i] & 0xFF);
+                    buffer.append(str.length() == 1 ? 0 + str : str);
+                }
+                strMacAddr = buffer.toString().toUpperCase();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return strMacAddr;
+        }catch (Exception e){
+            return null;
+        }
+
+    }
+
+    /**
+     * 获取移动设备本地IP
+     *
+     * @return
+     */
+    protected static InetAddress getLocalInetAddress() {
+        InetAddress ip = null;
+        try {
+            //列举
+            Enumeration en_netInterface = NetworkInterface.getNetworkInterfaces();
+            while (en_netInterface.hasMoreElements()) {//是否还有元素
+                NetworkInterface ni = (NetworkInterface) en_netInterface.nextElement();//得到下一个元素
+                Enumeration en_ip = ni.getInetAddresses();//得到一个ip地址的列举
+                while (en_ip.hasMoreElements()) {
+                    ip = (InetAddress) en_ip.nextElement();
+                    if (!ip.isLoopbackAddress() && ip.getHostAddress().indexOf(":") == -1)
+                        break;
+                    else
+                        ip = null;
+                }
+                if (ip != null) {
+                    break;
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return ip;
+    }
+}
---
+++ TheCodeToolsProject/readme.md
@@ -0,0 +1,61 @@
+## 相关工具类名称以及功能介绍
+
+### IpUtils
+
+ip地址相关的工具类
+
+### MacAddressUtils
+
+mac地址相关的工具类
+
+### SoManyUtils
+
+多个工具类的集合
+
+#### 功能介绍
+
+| 获取运营商名字                              | getOperatorName       |
+| ------------------------------------------- | --------------------- |
+| 获取imsi                                    | getImsi               |
+| 获取运营商编码                              | getOperatorCode       |
+| 获取系统版本号                              | getOsVersion          |
+| 获得app版本号                               | getAppCode            |
+| 获取应用程序名称                            | getAppName            |
+| 获得进程id                                  | getPID                |
+| 获得包名                                    | getPackageName        |
+| 获得手机品牌                                | getBrand              |
+| 获得手机型号                                | getBrandModel         |
+| 获得手机的机器码                            | getDeviceId           |
+| 获得手机分辨率  星号隔开                    | getScreenPixel        |
+| 获取用户使用的语言                          | getUsedLanguge        |
+| 当前使用内存占总内存的比例                  | heapPercent           |
+| 获得系统可用内存                            | getSysAvailableMemory |
+| 获取手机剩余存储空间                        | remainStorageSize     |
+| 获取内部存储空间                            | getSystemStorageSize  |
+| 手机gps是否开启                             | isGpsOpen             |
+| 获取屏幕方向                                | getScreenOrientation  |
+| 判断手机是否是竖向的 横向像素大于竖向未横向 | isScreenVertical      |
+
+| 判断是否开启了屏幕自动旋转 | isScreenAutoRotate |
+| -------------------------- | ------------------ |
+| 获得sdk版本                | getSdkVersion      |
+| 进行md5加密                | md5                |
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

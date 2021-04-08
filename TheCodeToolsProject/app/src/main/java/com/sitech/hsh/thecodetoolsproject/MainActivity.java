83ba908bce7393f2fbf8ef02021d3b076e727398
mage <1275054668@qq.com>
2021-04-08 14:30:15 +0800

    第一次提交工程

---
+++ TheCodeToolsProject/app/src/main/java/com/sitech/hsh/thecodetoolsproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sitech.hsh.thecodetoolsproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
